f80b460f4b20cd79c88cea5534b885cf66ba32b5
Promise Fru <promisefru1@gmail.com>
2020-10-29 13:23:41 +0100

    views setup

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + './views/index.html');
+})
+
+io.on('connection', socket => {
+    console.log('Client connected');
 })
 
 http.listen(port, console.log(`Server started on port ${port}`));
