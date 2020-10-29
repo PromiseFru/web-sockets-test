2c82b06ae634edb927281111b0a62c4bd27cb19d
Promise Fru <promisefru1@gmail.com>
2020-10-29 17:05:10 +0100

    updates

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var io = require('socket.io')(http);
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 io.on('connection', socket => {
     console.log('Client connected');
-})
+});
 
 http.listen(port, console.log(`Server started on port ${port}`));
